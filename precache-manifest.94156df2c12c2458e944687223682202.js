a62755e25e7360ec2432e6232504f318a03af271
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-05-28 14:10:21 +0300

    Updates

---
+++ precache-manifest.94156df2c12c2458e944687223682202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1cb8cce0b1a7ab666e58e03e9fa65f",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "4483f800aebff54cf5fd",
+    "url": "/Weather/static/css/main.aecf3e87.chunk.css"
+  },
+  {
+    "revision": "4c7dc8f186ce31085822",
+    "url": "/Weather/static/js/2.276d98d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.276d98d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4483f800aebff54cf5fd",
+    "url": "/Weather/static/js/main.4069f2d7.chunk.js"
+  },
+  {
+    "revision": "0356e636dd9d77987dda",
+    "url": "/Weather/static/js/runtime-main.ab064488.js"
+  }
+]);
